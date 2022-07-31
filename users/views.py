051cc207b493001c8cb9e08410cdfef0ef49e43e
kikikido <starararara@naver.com>
2022-08-01 00:00:49 +0900

    first commit

---
+++ users/views.py
@@ -0,0 +1,59 @@
+from django.http import HttpResponseRedirect
+from django.shortcuts import redirect, render
+from django.contrib.auth import authenticate, login, logout
+from .models import *
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(username=username, password=password) # user가 인증되면 user객체 반환, 인증되지 않으면 None객체 반환
+        if user is not None:
+            # A backend authenticated the credentials
+            print('인증성공')
+            login(request, user)
+        else:
+            # No backend authenticated the credentials
+            print('인증실패')
+            if not User.objects.filter(username=username).exists():
+                content = '아이디를 확인해주세요'
+            else:
+                content = '비밀번호를 확인해주세요'
+            return render(request, 'users/login.html', {'content':content})
+    return render(request, 'users/login.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('users:login')
+
+def signup_view(request):
+    if request.method == 'POST':
+        print(request.POST)
+
+
+        username = request.POST['username']
+        password = request.POST['password']
+        firstname = request.POST['firstname']
+        lastname = request.POST['lastname']
+        email = request.POST['email']
+        age = request.POST['age']
+
+        if User.objects.filter(username=username).exists():
+            return render(request, 'users/signup.html', {'content':'이미 사용중인 아이디입니다.'})
+        elif User.objects.filter(email=email).exists():
+            return render(request, 'users/signup.html', {'content':'이미 사용중인 이메일입니다.'})
+        
+        if username and email and password and firstname and lastname and int(age)>0:
+            user = User.objects.create_user(username, email, password)
+            user.first_name = firstname
+            user.last_name = lastname
+            user.age = age
+
+            user.save()
+            login(request, user)
+            return redirect('users:login')
+        else:
+            print(request.POST)
+            return render(request, 'users/signup.html', {'content':'양식을 제대로 입력하세요'})
+    return render(request, 'users/signup.html')
